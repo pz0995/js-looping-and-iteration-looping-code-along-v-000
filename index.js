11b2a1262231064c8cd61d3d69af6198258d1806
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-11 12:09:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+
+const employees = ["Ada", "Brendan", "Ali"];
+
+function printBadges(employees) {
+
+  for(let i = 0; i > employees.count; i++); {
+  console.log("Welcome ${employees}! You are employee #${employees[i]}.");
+  // console.log([i])
+  }
+//   return badges
+// }
+// printBadges(employees)
+}
+
+function tailsNeverFails() {
+
+   
+  let flip = 0;
+  let tails = 0;
+  let heads = 0;
+   
+  while (Math.random() >= 0.5) {
+    // Math.random();
+    // }
+    
+      if (Math.random() > 0.5) {
+        tails++
+
+        } else {
+        heads++    
+
+            console.log("You got ${flip} tails in a row!");
+    }
+    flip++
+ }
+}
